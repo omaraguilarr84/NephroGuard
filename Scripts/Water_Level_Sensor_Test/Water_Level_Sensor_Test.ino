a1981580c16570717d9a1d3c7119dcc46f8a9bfd
omaraguilarr84 <omar.hawks8@gmail.com>
2024-04-04 20:00:21 -0400

    float sensor working

--- Scripts/Water_Level_Sensor_Test/Water_Level_Sensor_Test.ino
+++ Scripts/Water_Level_Sensor_Test/Water_Level_Sensor_Test.ino
@@ -1,19 +1,23 @@
-int floatSensor = 8;
-int buttonState = 1;
+int floatSensor = 9;
+int buttonState;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(floatSensor, INPUT_PULLUP);
+  pinMode(floatSensor, INPUT);
 
 }
 
 void loop() {
+  buttonState = digitalRead(floatSensor);
+  
   if (buttonState == LOW)
-  {
-    Serial.println("Water Level - HIGH");
-  }
-  else
   {
     Serial.println("Water Level - LOW");
   }
+  else
+  {
+    Serial.println("Water Level - HIGH");
+  }
+
+  delay(1000);
 }
